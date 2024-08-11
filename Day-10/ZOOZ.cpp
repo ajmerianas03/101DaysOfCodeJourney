37ed49b54554f64a410e4d21dc6806f142bfa80e
Anas Ajmeri <ajmerianas03@gmail.com>
2024-08-11 18:28:48 +0530

    Day 10/101

---
+++ Day-10/ZOOZ.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int len; 
+    cin >> len;
+    
+  
+    cout << 1;  
+    for (int i = 0; i < len - 2; i++) {
+        cout << 0;  
+    }
+    cout << 1 << "\n";  
+}
+
+int main() {
+    int tc;
+    cin >> tc;  
+    
+    
+    while (tc--) {
+        solve();  
+    }
+    
+    return 0;
+}
