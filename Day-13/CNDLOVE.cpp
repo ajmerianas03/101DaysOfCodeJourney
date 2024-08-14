5c26446b8aef6022409eca800ff209d43771411a
Anas Ajmeri <ajmerianas03@gmail.com>
2024-08-14 21:29:45 +0530

    day 12

---
+++ Day-13/CNDLOVE.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+
+    int N, A, sum = 0, num;
+
+    cin >> N;
+
+    //  vector<int> A(N);
+
+    for (int i = 0; i < N; i++)
+    {
+        cin >> num;
+
+        sum += num;
+    }
+
+    if (sum & 1)
+    {
+        cout << "Yes" << endl;
+    }
+    else
+    {
+        cout << "No" << endl;
+    }
+}
+
+int main()
+{
+
+    int t;
+
+    cin >> t;
+
+    while (t--)
+    {
+        solve();
+    }
+}
