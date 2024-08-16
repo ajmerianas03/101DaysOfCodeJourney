5b5f5a07c61525c9f41dc6270441da6f3de05f68
Anas Ajmeri <ajmerianas03@gmail.com>
2024-08-16 22:33:18 +0530

    Day-15/101

---
+++ Day-15/TTENIS.cpp
@@ -0,0 +1,111 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define int long long int
+#define F first
+#define S second
+#define pb push_back
+#define si set<int>
+#define vi vector<int>
+#define vii vector<vi>
+#define pii pair<int,int>
+#define vpi vector<pii>
+#define vpp vector<pair<int,pii>
+#define mii map<int,int>
+#define spi set<pii>
+#define endl "\n"
+#define sz(x) (int()x.size())
+#define all(p) p.begin(),p.end()
+#define double long double
+#define que_max priority_queue<int>
+#define que_min priority_queue<int,vi,greater<int>>
+#define LOOP(n) for(int i=0;i<n;i++)
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define but(...) __f(#__VA_ARGS__,__VA_ARGS__)
+#define print(a) \
+    for (auto x : a) \
+        cout << x << " "; \
+    cout << endl
+#define print1(a) \
+    for(auto x:a) \
+    cout << x.F << " " <<x.S << endl
+#define print2(a,x,y) \
+    for(int i=x;i<y;i++) \
+        cout<<a[i] << " "; \
+    cout<<endl
+
+inline int power(int a,int b)
+{
+int x =1 ;
+while(b)
+{
+if(b & 1)
+     x*=a;
+a*=a;
+b >>= 1;
+}
+return x;
+}
+
+template <typename Arg1>
+void __f(const char *name, Arg1 &&arg1) { cout << name << ':' << arg1 <<endl; }
+template <typename Arg1, typename... Args>
+void __f(const char *names,Arg1 &&arg1, Args && ...args)
+{
+const char *comma = strchr(names+1, ',');
+cout.write(names,comma-names) << " : " << arg1 << " | ";
+__f(comma +1, args...);
+}
+
+const int N = 200005;
+void solve()
+{
+    string s;
+
+    cin >> s;
+
+    int len ,count1=0 ,count0=0 ;
+
+    len = s.length();
+
+    for (int i = 0; i < len; i++)
+    {
+        if (s[i]=='1')
+        {
+           
+           count1++;
+        }else
+        {
+            count0++;
+        }
+        
+        
+        
+    }
+
+    if (count1>count0)
+    {
+        cout << "WIN"<< endl;
+    }else
+    {
+        cout << "LOSE"<< endl;
+    }
+    
+    
+    
+}
+
+int32_t main()
+{
+
+clock_t z = clock();
+
+int t = 1;
+cin >> t;
+while(t--) solve();
+
+cerr << "Run Time : " << ((double)(clock()-z) / CLOCKS_PER_SEC);
+
+return 0;
+}
