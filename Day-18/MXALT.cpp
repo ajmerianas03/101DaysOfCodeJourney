a2268d7ee3a605536610728c9890aae7f74c73dc
Anas Ajmeri <ajmerianas03@gmail.com>
2024-08-19 13:35:10 +0530

    Day-18/101

---
+++ Day-18/MXALT.cpp
@@ -0,0 +1,118 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define int long long int
+#define F first
+#define S second
+#define pb push_back
+#define si set<int>
+#define vi vector<int>
+#define vii vector<vi>
+#define pii pair<int,int>
+#define vpi vector<pii>
+#define vpp vector<pair<int,pii>
+#define mii map<int,int>
+#define spi set<pii>
+#define endl "\n"
+#define sz(x) (int()x.size())
+#define all(p) p.begin(),p.end()
+#define double long double
+#define que_max priority_queue<int>
+#define que_min priority_queue<int,vi,greater<int>>
+#define LOOP(n) for(int i=0;i<n;i++)
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define but(...) __f(#__VA_ARGS__,__VA_ARGS__)
+#define print(a) \
+    for (auto x : a) \
+        cout << x << " "; \
+    cout << endl
+#define print1(a) \
+    for(auto x:a) \
+    cout << x.F << " " <<x.S << endl
+#define print2(a,x,y) \
+    for(int i=x;i<y;i++) \
+        cout<<a[i] << " "; \
+    cout<<endl
+
+inline int power(int a,int b)
+{
+int x =1 ;
+while(b)
+{
+if(b & 1)
+     x*=a;
+a*=a;
+b >>= 1;
+}
+return x;
+}
+
+template <typename Arg1>
+void __f(const char *name, Arg1 &&arg1) { cout << name << ':' << arg1 <<endl; }
+template <typename Arg1, typename... Args>
+void __f(const char *names,Arg1 &&arg1, Args && ...args)
+{
+const char *comma = strchr(names+1, ',');
+cout.write(names,comma-names) << " : " << arg1 << " | ";
+__f(comma +1, args...);
+}
+
+const int N = 200005;
+void solve()
+{
+    // int N ;
+    // cin >> N ;
+
+    // vi arr(N);
+    // LOOP(N) cin >> arr[i];
+
+    // int ans=arr[0];
+
+    // for (int i = 1; i < N; i++)
+    // {
+    //     if (i&1)
+    //     {
+    //         ans +=arr[i];
+    //     }else
+    //     {
+    //         ans -=arr[i];
+    //     }
+
+        
+        
+        
+    // }
+    // cout << ans << endl;
+
+        int n;
+    cin>>n;
+    vi arr(n);
+    for (int i = 0; i < n; i++) {
+        cin>>arr[i];
+    }
+    sort(arr.begin(),arr.end());
+    int sum_odd = 0, sum_even = 0;
+    for (int i = 0; i < n/2; i++) {
+        sum_even += arr[i];
+    }
+    for (int i = n/2; i < n; i++) {
+        sum_odd += arr[i];
+    }
+    cout<<sum_odd - sum_even<<endl;
+    
+}
+
+int32_t main()
+{
+
+clock_t z = clock();
+
+int t = 1;
+cin >> t;
+while(t--) solve();
+
+cerr << "Run Time : " << ((double)(clock()-z) / CLOCKS_PER_SEC);
+
+return 0;
+}
